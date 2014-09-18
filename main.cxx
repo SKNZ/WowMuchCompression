c9a45acd3151e37dd58892df0dacfd018d1d1ec4
sknz <florandara@gmail.com>
2014-09-18 15:20:42 +0200

    Pixel class, doxygen header

--- main.cxx
+++ main.cxx
@@ -1,9 +1,34 @@
 /**
- * 
- * @Author: matie toma é flor
- */
+ * @file main.cxx
+ * @author NARENJI-SHESHKALANI Floran
+ * @author MERINO Mathieu
+ * @author MARGIER Thomas
+ * @date 18/09/2014
+ * @brief WMC Video Compression Codec
+ *
+ **/
+
+namespace
+{
+	class Pixel
+	{
+	public:
+		Pixel(int r, int g, int b) : m_r(r), m_g(g), m_b(b) { }
+
+		int getR() { return m_r; }
+		int getG() { return m_g; }
+		int getB() { return m_b; }
+
+		void setR(int r) { m_r = r; }
+		void setG(int g) { m_g = g; }
+		void setB(int b) { m_b = b; }
+
+	private:
+		int m_r, m_g, m_b;
+	};
+}
 
 int main(int argc, char** argv)
 {
-qsdcqd
+
 }
