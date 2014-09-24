a37da7d8c18157cb95e41a47520d4372029154d0
Percee <percee@live.fr>
2014-09-24 14:07:37 +0200

    CTranslation, comments addition, continued compress ()

--- main.cxx
+++ main.cxx
@@ -14,6 +14,7 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <map>
 
 #define PIXEL_BLOCK_SIZE 		5
 #define PIXEL_MAX_COLOR_DELTA	5
@@ -23,11 +24,13 @@ using namespace std;
 namespace
 {
 	class CPixel;
+	class CTranslation;
 	
 	typedef vector <CPixel> CPixelColumn;
 	typedef vector <CPixelColumn> CFrame;
 	typedef vector <CFrame> CVideo;
 	
+	map <int, vector <CTranslation>> Map;
 	CVideo Video;
 	
 	class CPixel
@@ -35,6 +38,8 @@ namespace
 	public:
 		CPixel(int r = 0, int g = 0, int b = 0) : R(r), G(g), B(b) {}
 		
+		/*check red, blue and green of *this and a target pixel, if the difference is bigger than MAX_COLOR_DELTA,
+		return false and consider they are not enough resemblant*/
 		bool compare (const CPixel & tar) const
 		{
 			return (tar.R <= R + PIXEL_MAX_COLOR_DELTA || tar.R >= R - PIXEL_MAX_COLOR_DELTA) &&
@@ -43,23 +48,32 @@ namespace
 		}
 		int R, G, B;
 	};
+	
+	class CTranslation
+	{
+	public:
+		int X1, Y1, X2, Y2; //X1,Y1 top left corner of block on current frame. X2, Y2 top left corner of block on current frame +1
+	};
 
 	void printErrorMessage ()
 	{
 		cerr << "Usage: wmc -[c|x] fileName" << endl;
 	}
 	
+	// compare two matrix of pixels, checking if they are alike depending on % difference pixel per pixel
 	bool comparePixelBlocks (const CFrame & pixelBlockRef, const CFrame & pixelBlockTar)
 	{
+		// double for the check every pixel of every line
 		for (int x = 0; x < PIXEL_BLOCK_SIZE; ++x)
 		{
 			for (int y = 0; y < PIXEL_BLOCK_SIZE; ++y)
 			{
+				// if even only two pixels of same positions in the 2 matrix are more than X% different, return false
 				if (!pixelBlockRef [x][y].compare (pixelBlockTar [x][y]))
 					return false;
 			}
 		}
-		return true;
+		return true; // if every equivalent pixel are alike, return true
 	}
 
 	bool loadFile (char* fileName)
@@ -76,27 +90,33 @@ namespace
 
 	void compress()
 	{
-		//Redondance avec vecteurs
-		for (int frameNumber = 0; frameNumber < Video.size () - 1; ++frameNumber)
+		// redundancy
+		for (unsigned frameNumber = 0; frameNumber < Video.size () - 1; ++frameNumber)// every frame of the video
 		{
-			CFrame & frameRef = Video [frameNumber];
-			CFrame pixelBlockRef;
+			CFrame & frameRef = Video [frameNumber];// reference frame which will be used to compress target frame
+			CFrame & frameTar = Video [frameNumber + 1];// target frame which will be compressed
+			CFrame pixelBlockRef;//	block of pixel from reference frame that will be compared to every block of pixel on target frame
 			pixelBlockRef.resize (PIXEL_BLOCK_SIZE);
 					
 			for (CPixelColumn Column : pixelBlockRef)
-			Column.resize (PIXEL_BLOCK_SIZE);
+				Column.resize (PIXEL_BLOCK_SIZE);
 					
 			CFrame pixelBlockTar (pixelBlockRef); //PixelBlockTarget
-			for (int x = 0; x < Video [0].size () - PIXEL_BLOCK_SIZE + 1; ++x)
-			{
-				for (int y = 0; y < Video [0][0].size () - PIXEL_BLOCK_SIZE + 1; ++y)
-				{
-					for (int xBlock = 0; x < pixelBlockRef.size (); ++x)
-						for (int yBlock = 0; y < pixelBlockRef.size (); ++y)
-							pixelBlockRef [xBlock][yBlock] = frameNumber [x][y];
-
-				}
-			}
+			// fill pixelBlockRef with pixels from reference frame
+			// for each pixel of the frame
+			for (unsigned x = 0; x < Video [0].size () /*width of a frame*/ - PIXEL_BLOCK_SIZE + 1; ++x)
+				for (unsigned y = 0; y < Video [0][0].size () /*height of a frame*/ - PIXEL_BLOCK_SIZE + 1; ++y)
+					// for each pixel of the block
+					for (unsigned xBlock = 0; x < pixelBlockRef.size (); ++x)
+						for (unsigned yBlock = 0; y < pixelBlockRef.size (); ++y)
+							pixelBlockRef [xBlock][yBlock] = frameRef[x + xBlock][y + yBlock];// fill the block
+			// fill pixelBlockTar with pixels from target frame
+			for (unsigned x = 0; x < Video [0].size () /*width of a frame*/ - PIXEL_BLOCK_SIZE + 1; ++x)
+				for (unsigned y = 0; y < Video [0][0].size () /*height of a frame*/ - PIXEL_BLOCK_SIZE + 1; ++y)
+					for (unsigned xBlock = 0; x < pixelBlockRef.size (); ++x)
+						for (unsigned yBlock = 0; y < pixelBlockRef.size (); ++y)
+							pixelBlockTar [xBlock][yBlock] = frameTar [x + xBlock][y + yBlock];// fill the block
+			
 		}
 		
 		
